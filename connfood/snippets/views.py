6c7aaa324d4d1acf1059199668e5d070cb357ffc
user <fouchegaspard@gmail.com>
2020-04-06 19:29:52 +0200

    Created views for the API.
    Added the following views into snippets/views.py. All of the views use generics built-in django views
    in order to reduce the amount of code:
    
    - FarmerList / FarmerDetail will respectively display a list of all the farmers, and selected farmer related details.
    - ProductList / ProductDetail will respectively display a list of all the products, and selected product related details.
    - CertificateList / CertificateDetail will respectively display a list of all the certificates, and selected certificate related details.
    - UserList / UserDetail will respectively display a list of all users, and selected certificate related details.

--- connfood/snippets/views.py
+++ connfood/snippets/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import permissions
+from snippets.models import Farmer, Product, Certificate
+from snippets.serializers import FarmerSerializer, ProductSerializer, CertificateSerializer, UserSerializer
+from snippets.permissions import IsOwnerOrReadOnly
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+class FarmerList(generics.ListCreateAPIView):
+    queryset = Farmer.objects.all()
+    serializer_class = FarmerSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class FarmerDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Farmer.objects.all()
+    serializer_class = FarmerSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
+
+
+class ProductList(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
+
+
+class CertificateList(generics.ListCreateAPIView):
+    queryset = Certificate.objects.all()
+    serializer_class = CertificateSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class CertificateDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Certificate.objects.all()
+    serializer_class = CertificateSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
