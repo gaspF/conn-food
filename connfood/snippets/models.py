d8cd29f863288a0f3915d46825524efab690a783
user <fouchegaspard@gmail.com>
2020-04-06 16:54:27 +0200

    Added models to snippets/models.py, and made migrations to the database.
    In snippets/models.py :
    - Added Certificate class related to Farmer model by a ForeignKey (certified_farmer)
    - Added Farmer class
    - Added Product class related to Farmer model by a many to many relation.

--- connfood/snippets/models.py
+++ connfood/snippets/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
-# Create your models here.
+CERTIFICATE_CHOICES = (
+    ('BIOLOGIQUE', 'Biologique'),
+    ('SANS_OGM', 'Sans OGM'),
+    ('ORIGINE', 'Origine'),
+)
+
+
+class Farmer(models.Model):
+    owner = models.ForeignKey('auth.User', related_name='farmers', on_delete=models.CASCADE)
+    farmer_creation_date = models.DateTimeField(auto_now_add=True)
+    farmer_name = models.CharField(max_length=100, blank=False)
+    farmer_siret_number = models.PositiveIntegerField(blank=False)
+    farmer_address = models.TextField(blank=False)
+
+    class Meta:
+        ordering = ['farmer_creation_date']
+
+    def __str__(self):
+        return '{}'.format(self.farmer_name)
+
+
+class Product(models.Model):
+    owner = models.ForeignKey('auth.User', related_name='products', on_delete=models.CASCADE)
+    product_creation_date = models.DateTimeField(auto_now_add=True)
+    product_name = models.CharField(max_length=100, blank=False)
+    product_unit = models.PositiveIntegerField(blank=False)
+    international_food_code = models.TextField(max_length=15, blank=False)
+    producers = models.ManyToManyField(Farmer)
+
+    class Meta:
+        ordering = ['product_creation_date']
+
+    def __str__(self):
+        return '{} {} {} {}'.format(self.product_name, self.product_unit, self.international_food_code, self.producers)
+
+
+class Certificate(models.Model):
+    owner = models.ForeignKey('auth.User', related_name='certificates', on_delete=models.CASCADE)
+    certificate_creation_date = models.DateTimeField(auto_now_add=True)
+    certificate_name = models.CharField(max_length=100, blank=False)
+    certificate_type = models.CharField(max_length=10, default='ORIGINE', choices=CERTIFICATE_CHOICES)
+    certified_farmer = models.ForeignKey(Farmer, on_delete=models.CASCADE,)
+
+    class Meta:
+        ordering = ['certificate_creation_date']
+
+    def __str__(self):
+        return '{}: {} {} {}'.format(self.certificate_creation_date, self.certified_farmer, self.certificate_name, self.certificate_type.name)
