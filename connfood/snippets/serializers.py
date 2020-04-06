886e9d85d1cd09e4ad2b97486f705ec1a1c37282
user <fouchegaspard@gmail.com>
2020-04-06 18:51:37 +0200

    Added serializers to snippets/serializers.py.
    In snippets/serializers.py :
    - Added UserSerializer, which is related to all of the models (Farmer, Product, Certificate) by a foreign key, in order to link all of the API's posted data to an author.
    - Added FarmerSerializer, which has been created to serialize Farmer's model data.
    - Added ProductSerializer, which has been created to serialize Product's model data.
    - Added CertificateSerializer, which has been created to serialize Certificate's model data.

---
+++ connfood/snippets/serializers.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+from snippets.models import Farmer, Product, Certificate, CERTIFICATE_CHOICES
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    farmers = serializers.PrimaryKeyRelatedField(many=True, queryset=Farmer.objects.all())
+    products = serializers.PrimaryKeyRelatedField(many=True, queryset=Product.objects.all())
+    certificates = serializers.PrimaryKeyRelatedField(many=True, queryset=Certificate.objects.all())
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class FarmerSerializer(serializers.ModelSerializer):
+    owner = serializers.SlugRelatedField(slug_field='username', queryset=User.objects.all())
+
+    class Meta:
+        model = Farmer
+        fields = '__all__'
+
+
+class CertificateSerializer(serializers.ModelSerializer):
+    certified_farmer = serializers.SlugRelatedField(slug_field='farmer_name', queryset=Farmer.objects.all())
+    owner = serializers.SlugRelatedField(slug_field='username', queryset=User.objects.all())
+    certificate_type = serializers.ChoiceField(choices=CERTIFICATE_CHOICES, default='ORIGINE')
+
+    class Meta:
+        model = Certificate
+        fields = '__all__'
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    owner = serializers.SlugRelatedField(read_only=True, slug_field='username')
+    producers = serializers.SlugRelatedField(many=True, slug_field='farmer_name', queryset=Farmer.objects.all())
+
+    class Meta:
+        model = Product
+        fields = '__all__'
