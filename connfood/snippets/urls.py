5eed217a76775da9df89789cce977b6846b32ed7
user <fouchegaspard@gmail.com>
2020-04-07 15:38:11 +0200

    Updated snippets/urls.py by binding viewsets classes into concrete views
    The following views have been created and binded to a variable :
    - FarmerViewSet.as_view() to farmer_list with get/post methods and
    farmer_detail with get/put/patch/delete methods.
    - ProductViewSet.as_view() to product_list with get/post methods and
    product_detail with get/put/patch/delete methods.
    - CertificateViewSet.as_view() to certificate_list with get/post
    methods and certificate_detail with get/put/patch/delete methods.
    
    Modified the attached URL by associating them with their respective views.

--- connfood/snippets/urls.py
+++ connfood/snippets/urls.py
@@ -1,16 +1,56 @@
 from django.urls import path
 from snippets import views
+from snippets.views import FarmerViewSet, UserViewSet, ProductViewSet, CertificateViewSet
 from rest_framework.urlpatterns import format_suffix_patterns
+
+
+farmer_list = FarmerViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+farmer_detail = FarmerViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+product_list = ProductViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+product_detail = ProductViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+certificate_list = CertificateViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+certificate_detail = CertificateViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+user_list = UserViewSet.as_view({
+    'get': 'list'
+})
+user_detail = UserViewSet.as_view({
+    'get': 'retrieve'
+})
 
 
 urlpatterns = format_suffix_patterns([
     path('', views.api_root),
-    path('farmers/', views.FarmerList.as_view(), name='farmer-list'),
-    path('farmers/<int:pk>/', views.FarmerDetail.as_view(), name='farmer-detail'),
-    path('products/', views.ProductList.as_view(), name='product-list'),
-    path('products/<int:pk>/', views.ProductDetail.as_view(), name='product-detail'),
-    path('certificates/', views.CertificateList.as_view(), name='certificate-list'),
-    path('certificates/<int:pk>/', views.CertificateDetail.as_view(), name='certificate-detail'),
-    path('users/', views.UserList.as_view(), name='user-list'),
-    path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail')
+    path('farmers/', farmer_list, name='farmer-list'),
+    path('farmers/<int:pk>/', farmer_detail, name='farmer-detail'),
+    path('products/', product_list, name='product-list'),
+    path('products/<int:pk>/', product_detail, name='product-detail'),
+    path('certificates/', certificate_list, name='certificate-list'),
+    path('certificates/<int:pk>/', certificate_detail, name='certificate-detail'),
+    path('users/', user_list, name='user-list'),
+    path('users/<int:pk>/', user_detail, name='user-detail')
 ])
+
