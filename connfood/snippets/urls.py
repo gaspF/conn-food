1bdaed0f4f8432ec5d63c7b7f9f99f89e23edb54
user <fouchegaspard@gmail.com>
2020-04-07 14:45:00 +0200

    Updated snippets/urls.py by naming the following API URL patterns:
    - farmer-list
    - farmer-detail
    - product-list
    - product-detail
    - certificate-list
    - certificate-detail
    - user-list
    - user-detail

--- connfood/snippets/urls.py
+++ connfood/snippets/urls.py
@@ -3,16 +3,14 @@ from snippets import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 
-urlpatterns = [
+urlpatterns = format_suffix_patterns([
     path('', views.api_root),
-    path('farmers/', views.FarmerList.as_view()),
-    path('farmers/<int:pk>/', views.FarmerDetail.as_view()),
-    path('products/', views.ProductList.as_view()),
-    path('products/<int:pk>/', views.ProductDetail.as_view()),
-    path('certificates/', views.CertificateList.as_view()),
-    path('certificates/<int:pk>/', views.CertificateDetail.as_view()),
-    path('users/', views.UserList.as_view()),
-    path('users/<int:pk>/', views.UserDetail.as_view()),
-]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
+    path('farmers/', views.FarmerList.as_view(), name='farmer-list'),
+    path('farmers/<int:pk>/', views.FarmerDetail.as_view(), name='farmer-detail'),
+    path('products/', views.ProductList.as_view(), name='product-list'),
+    path('products/<int:pk>/', views.ProductDetail.as_view(), name='product-detail'),
+    path('certificates/', views.CertificateList.as_view(), name='certificate-list'),
+    path('certificates/<int:pk>/', views.CertificateDetail.as_view(), name='certificate-detail'),
+    path('users/', views.UserList.as_view(), name='user-list'),
+    path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail')
+])
